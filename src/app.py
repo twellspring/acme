a08cc7ff3d4a031d9eb62637e2c1f0dd6426dfc3
Todd Wells <todd@wells.ws>
2023-01-28 10:42:19 -0800

    fix: update HTML (#6)

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><center><h1>Acme Company<h1><h2>Anvil Support Portal</h2><h3>Is your anvil not working correctly? We can help you fix it!!!</h3><img src='https://i.pinimg.com/736x/fb/f4/39/fbf439f72e30ed5924c78161e1b34589--bugs-bunny-bronze-sculpture.jpg'></center></body></html>  "
+    return "<html><body><center><h1>Acme Company<h1><h2>Anvil Support Portal</h2><h3>Is your anvil not working correctly? We can help you fix it!!!!</h3><img src='https://i.pinimg.com/736x/fb/f4/39/fbf439f72e30ed5924c78161e1b34589--bugs-bunny-bronze-sculpture.jpg'></center></body></html>  "
 
 
 if __name__ == "__main__":
