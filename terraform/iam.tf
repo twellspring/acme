d3b94621e374fa1e4544d1d01322d0a3abbb2693
Todd Wells <todd@wells.ws>
2023-01-30 15:09:35 -0800

    fix: fix_iam (#26)

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_role" {
-  name               = "${var.application}-EcsTaskRole"
+  name               = "${local.prefix}-EcsTaskRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
